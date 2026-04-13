0c86e9d98c947e97225ec2e346e56f7d0446b911
Tetiana Zinovieva <talia.97.it@gmail.com>
2026-04-13 17:53:34 +0200

    add error function and update import fetchNoteById

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -2,14 +2,16 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from 'next/navigation';
-import { getSingleNote } from "@/lib/api";
+import { fetchNoteById } from "@/lib/api";
+
+import css from '@/components/NotesPage/NotesPage.module.css'
 
 const NoteDetailsClient = () => {
 	const { id } = useParams<{ id: string }>();
 
   const { data: note, isLoading, error } = useQuery({
     queryKey: ["note", id],
-    queryFn: () => getSingleNote(id),
+    queryFn: () => fetchNoteById (id),
     refetchOnMount: false,
   });
 
@@ -17,9 +19,9 @@ const NoteDetailsClient = () => {
 
   if (error || !note) return <p>Some error..</p>;
 
-  const formattedDate = note.updatedAt
-    ? `Updated at: ${note.updatedAt}`
-    : `Created at: ${note.createdAt}`;
+  // const formattedDate = note.updatedAt
+  //   ? `Updated at: ${note.updatedAt}`
+  //   : `Created at: ${note.createdAt}`;
 
   return (
    <div className={css.container}>
--- app/notes/[id]/error.tsx
+++ app/notes/[id]/error.tsx
@@ -1,1 +1,16 @@
-<p>Could not fetch note details. {error.message}</p>
+"use client";
+
+type ErrorProps = {
+  error: Error;
+};
+
+export default function Error({
+  error,
+}: ErrorProps) {
+  return (
+    <p>
+      Could not fetch note details.{" "}
+      {error.message}
+    </p>
+  );
+}
